bb5a6c198364eaa5bd53e633f45629c6f8fd0284
Juan Felipe Rodriguez <133280694+felipe97rg@users.noreply.github.com>
2023-11-28 20:16:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,86 @@ import pandas as pd
 
 app = FastAPI()
 
+df_PlayTimeGenre = pd.read_parquet("df_PlayTimeGenre.parquet")
+df_UserForGenre = pd.read_parquet("df_UserForGenre.parquet")
+df_UserRecommend = pd.read_parquet("df_UserRecommend.parquet")
+df_UserWorstDeveloper = pd.read_parquet("df_UserWorstDeveloper.parquet")
 df_Sentiment_Analysis = pd.read_parquet("df_Sentiment_Analysis.parquet")
 
+
+# Endpoint de la función PlayTimeGenre: Debe devolver año con mas horas jugadas para dicho género. Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}
+@app.get('/PlayTimeGenre')
+def PlayTimeGenre(genero : str):
+    # Filtrar el DataFrame para obtener solo las filas que contienen el género específico
+    filtro_genero = df_PlayTimeGenre['genres'].str.contains(genero, case=False, na=False)
+    df_filtrado = df_PlayTimeGenre[filtro_genero]
+    df_agrupado = df_filtrado.groupby('year')['playtime_forever'].sum().reset_index()
+    # Encontrar el año con la mayor suma de 'playtime_forever'
+    df_agrupado['year'] = df_agrupado['year'].astype(int)
+    anio_mayor_suma = df_agrupado.loc[df_agrupado['playtime_forever'].idxmax(), 'year']
+    respuesta = {f"Año de lanzamiento con más horas jugadas para Género el {genero}": anio_mayor_suma}
+    return respuesta 
+
+
+@app.get('/UserForGenre')
+def UserForGenre(genero: str):
+    # Filtrar el DataFrame para el género especificado
+    filtro_genero = df_UserForGenre['genres'].str.contains(genero, case=False, na=False)
+    df_filtrado = df_UserForGenre[filtro_genero]
+
+    # Agrupar por 'user_id' y 'year', sumar las horas jugadas
+    df_agrupado = df_filtrado.groupby(['user_id', 'year'])['playtime_forever'].sum().reset_index()
+
+    # Encontrar el usuario con la máxima suma de horas jugadas
+    idx_max_playtime = df_agrupado['playtime_forever'].idxmax()
+    usuario_max_playtime = df_agrupado.loc[idx_max_playtime, 'user_id']
+
+    # Filtrar el DataFrame para el usuario con máxima suma de horas jugadas
+    df_usuario = df_agrupado[df_agrupado['user_id'] == usuario_max_playtime]
+
+    # Crear el formato "Horas jugadas"
+    resultado_final = [{'Año': int(row['year']), 'Horas': int(row['playtime_forever'])} for _, row in df_usuario.iterrows()]
+    
+    return {"Usuario con más horas jugadas para Género {}:".format(genero): usuario_max_playtime, "Horas jugadas": resultado_final}
+
+
+@app.get('/UsersRecommend')
+def UsersRecommend(año: int):
+    
+    df = df_UserRecommend[(df_UserRecommend['year_posted'] == año) & (df_UserRecommend['recommend'] == True)]
+
+    # Contar el número de recomendaciones para cada juego
+    top_juegos = df['title'].value_counts().head(3)
+
+    # Crear el formato de salida
+    resultado_final = [{"Puesto {}: {}".format(i+1, juego): recomendaciones} for i, (juego, recomendaciones) in enumerate(top_juegos.items())]
+
+    return resultado_final
+
+
+@app.get('/UsersWorstDeveloper')
+def UserWorstDeveloper(year):
+    # Filtrar el DataFrame por el año especificado
+    df_filtered = df_UserWorstDeveloper[df_UserWorstDeveloper['year_posted'] == year]
+
+    # Agrupar y contar por desarrollador
+    developer_counts = df_filtered['developer'].value_counts().reset_index()
+    developer_counts.columns = ['developer', 'count']
+
+    # Ordenar por la cantidad en orden descendente
+    developer_counts = developer_counts.sort_values(by='count', ascending=False)
+
+    # Tomar los primeros tres resultados
+    top_three_developers = developer_counts.head(3)
+
+    # Crear la lista con el formato solicitado
+    result_list = [{"Puesto {}: {}".format(i + 1, row['developer']): row['count']} for i, row in top_three_developers.iterrows()]
+
+    return result_list
+
+
 @app.get('/Sentiment_Analysis')
-def Sentiment_Analysis(desarrollador):
+def Sentiment_analysis(desarrollador):
     # Filtra el DataFrame para obtener la fila correspondiente al desarrollador
     desarrollador =desarrollador.lower()
     desarrollador_fila = df_Sentiment_Analysis[df_Sentiment_Analysis['Developer'] == desarrollador]
